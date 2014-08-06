cf0e57c206cdbd64809f50448c72d3ed064045c8
Dave Guarino <dave@codeforamerica.org>
2014-08-06 13:57:44 -0700

    Pass more transcription versions and remove private search methods

--- lib/transcription.rb
+++ lib/transcription.rb
@@ -3,16 +3,8 @@ class Transcription
 
   def initialize(raw_text)
     @raw_text = raw_text
-    @ebt_amount = extract_ebt_amount
-    @cash_amount = extract_cash_amount
-  end
-
-  private
-  def extract_ebt_amount
-    @raw_text.match(/balance is (.+) your cash/)[1]
-  end
-
-  def extract_cash_amount
-    @raw_text.match(/cash account balance is (.+) as/)[1]
+    regex_matches = @raw_text.scan(/(\$\S+)/)
+    @ebt_amount = regex_matches[0][0]
+    @cash_amount = regex_matches[1][0]
   end
 end
