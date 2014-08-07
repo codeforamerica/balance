a5cbcab9f71de23e996668cf8a29c68e9b09d4d6
Dave Guarino <dave@codeforamerica.org>
2014-08-06 23:44:23 -0700

    Convert first two contexts to single nested describe block

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,52 +1,54 @@
 require 'spec_helper'
 
 describe EbtBalanceSmsApp do
-  context 'initial text with valid EBT number' do
-    let(:ebt_number) { "1111222233334444" }
-    let(:texter_number) { "+12223334444" }
-    let(:fake_twilio) { double("FakeTwilioService", :make_call => 'made call', :send_text => 'sent text') }
+  describe 'initial text' do
+    context 'with valid EBT number' do
+      let(:ebt_number) { "1111222233334444" }
+      let(:texter_number) { "+12223334444" }
+      let(:fake_twilio) { double("FakeTwilioService", :make_call => 'made call', :send_text => 'sent text') }
 
-    before do
-      allow(TwilioService).to receive(:new).and_return(fake_twilio)
-      post '/', { "Body" => ebt_number, "From" => texter_number }
+      before do
+        allow(TwilioService).to receive(:new).and_return(fake_twilio)
+        post '/', { "Body" => ebt_number, "From" => texter_number }
+      end
+
+      it 'initiates an outbound Twilio call to EBT line with correct details' do
+        expect(fake_twilio).to have_received(:make_call).with(
+          url: "http://example.org/get_balance?phone_number=#{texter_number}",
+          to: '+18773289677',
+          send_digits: "ww1ww#{ebt_number}",
+          from: 'loltwilionumber',
+          record: 'true',
+          method: 'GET'
+        )
+      end
+
+      it 'sends a text to the user telling them wait time' do
+        expect(fake_twilio).to have_received(:send_text).with(
+          to: texter_number,
+          from: 'loltwilionumber',
+          body: "Thanks! Please wait 1-2 minutes while we check your EBT balance."
+        )
+      end
     end
 
-    it 'initiates an outbound Twilio call to EBT line with correct details' do
-      expect(fake_twilio).to have_received(:make_call).with(
-        url: "http://example.org/get_balance?phone_number=#{texter_number}",
-        to: '+18773289677',
-        send_digits: "ww1ww#{ebt_number}",
-        from: 'loltwilionumber',
-        record: 'true',
-        method: 'GET'
-      )
-    end
+    context 'with INVALID EBT number' do
+      let(:invalid_ebt_number) { "111122223333" }
+      let(:texter_number) { "+12223334444" }
+      let(:fake_twilio) { double("FakeTwilioService", :make_call => 'made call', :send_text => 'sent text') }
 
-    it 'sends a text to the user telling them wait time' do
-      expect(fake_twilio).to have_received(:send_text).with(
-        to: texter_number,
-        from: 'loltwilionumber',
-        body: "Thanks! Please wait 1-2 minutes while we check your EBT balance."
-      )
-    end
-  end
+      before do
+        allow(TwilioService).to receive(:new).and_return(fake_twilio)
+        post '/', { "Body" => invalid_ebt_number, "From" => texter_number }
+      end
 
-  context 'initial text with INVALID EBT number' do
-    let(:invalid_ebt_number) { "111122223333" }
-    let(:texter_number) { "+12223334444" }
-    let(:fake_twilio) { double("FakeTwilioService", :make_call => 'made call', :send_text => 'sent text') }
-
-    before do
-      allow(TwilioService).to receive(:new).and_return(fake_twilio)
-      post '/', { "Body" => invalid_ebt_number, "From" => texter_number }
-    end
-
-    it 'sends a text to the user with error message' do
-      expect(fake_twilio).to have_received(:send_text).with(
-        to: texter_number,
-        from: 'loltwilionumber',
-        body: "Sorry, that EBT number doesn't look right. Please try again."
-      )
+      it 'sends a text to the user with error message' do
+        expect(fake_twilio).to have_received(:send_text).with(
+          to: texter_number,
+          from: 'loltwilionumber',
+          body: "Sorry, that EBT number doesn't look right. Please try again."
+        )
+      end
     end
   end
 end
