cb9fe248e50fa89c8b5f176913787896e5f4f995
Dave Guarino <dave@codeforamerica.org>
2014-08-14 19:14:33 -0700

    Add production credentials

--- twilio_console.rb
+++ twilio_console.rb
@@ -2,6 +2,7 @@ require 'twilio-ruby'
 require 'pry'
 
 staging_client = Twilio::REST::Client.new(ENV['TWILIO_BALANCE_STAGING_SID'], ENV['TWILIO_BALANCE_STAGING_AUTH'])
+production_client = Twilio::REST::Client.new(ENV['TWILIO_BALANCE_PROD_SID'], ENV['TWILIO_BALANCE_PROD_AUTH'])
 
 binding.pry
 
