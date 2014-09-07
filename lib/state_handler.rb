1dd11d206040d7c4afc18e0f3a65c8c649428d19
Dave Guarino <dave@codeforamerica.org>
2014-09-07 16:17:01 -0700

    Add transcriber for MA

--- lib/state_handler.rb
+++ lib/state_handler.rb
@@ -134,11 +134,67 @@ module StateHandler::MA
     [18]
   end
 
-  # A method that takes a transcription and returns EITHER:
-  # 1. A message with the balance, OR
-  # 2. A message that the system could not find the balance
-  def transcribe_balance_response(transcription_text)
-    transcription_text
+  def transcriber_for(language)
+    BalanceTranscriber.new(language)
+  end
+
+  class BalanceTranscriber
+    attr_reader :language
+
+    def initialize(language)
+      @language = language
+      if language == :spanish
+        extend SpanishTranscriptionMessages
+      else
+        extend EnglishTranscriptionMessages
+      end
+    end
+
+    def transcribe_balance_response(transcription_text)
+      return transcription_text
+=begin
+      if transcription_text == nil
+        return having_trouble_try_again_message
+      end
+      regex_matches = transcription_text.scan(/(\$\S+)/)
+      if transcription_text.include?("say I don't have it")
+        card_number_not_found_message
+      elsif regex_matches.count > 0
+        ebt_amount = regex_matches[0][0]
+        balance_message_for(ebt_amount)
+      else
+        having_trouble_try_again_message
+      end
+=end
+    end
+
+    module EnglishTranscriptionMessages
+      def having_trouble_try_again_message
+        "I'm really sorry! We're having trouble contacting the EBT system right now. Please text your EBT # again in a few minutes."
+      end
+
+      def card_number_not_found_message
+        "I'm sorry, that card number was not found. Please try again."
+      end
+
+      def balance_message_for(ebt_amount)
+        "Hi! Your food stamp balance is #{ebt_amount}."
+      end
+    end
+
+    module SpanishTranscriptionMessages
+      def having_trouble_try_again_message
+        "Lo siento! Actualmente estamos teniendo problemas comunicándonos con el sistema de EBT. Favor de enviar su # de EBT por texto en unos minutos."
+      end
+
+      def card_number_not_found_message
+        "Lo siento, no se encontró el número de tarjeta. Por favor, inténtelo de nuevo."
+      end
+
+      def balance_message_for(ebt_amount)
+        "Hola! El saldo de su cuenta de estampillas para comida es #{ebt_amount}."
+      end
+    end
   end
 end
 
