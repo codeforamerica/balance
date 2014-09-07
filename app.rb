7cf715523b3923a5dd7cc35534bae4986a02f9c7
Dave Guarino <dave@codeforamerica.org>
2014-09-07 13:40:47 -0700

    Replace voice message with better message, more generic, closes #111

--- app.rb
+++ app.rb
@@ -106,7 +106,7 @@ EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <Response>
   <Gather timeout="10" action="http://twimlets.com/forward?PhoneNumber=#{state_handler.phone_number}" method="GET" numDigits="1">
-    <Play>https://s3-us-west-1.amazonaws.com/balance-cfa/balance-splash.mp3</Play>
+    <Play>https://s3-us-west-1.amazonaws.com/balance-cfa/balance-voice-splash-v2-090714.mp3</Play>
   </Gather>
   <Redirect method="GET">http://twimlets.com/forward?PhoneNumber=#{state_handler.phone_number}</Redirect>
 </Response>
