79ab07735a806cd8bb724555759e378eaa0a9d60
Dave Guarino <dave@codeforamerica.org>
2014-10-24 14:47:27 -0700

    Add Amazon offer to Thanks message, v3 for #166, closes #191

--- lib/message_generator.rb
+++ lib/message_generator.rb
@@ -9,7 +9,7 @@ class MessageGenerator
     if language == :spanish
       "Gracias! Favor de esperar 1-2 minutos mientras verificamos su saldo de EBT."
     else
-      "Thanks! Please wait 1-2 minutes while we check your EBT balance. Want to help us make this better? Send a text to (415) 877-4154 and answer 3 quick questions."
+      "Thanks! Please wait 1-2 min while we check your balance. Want to help make this better & earn $5 in Amazon credit? Text 415-877-4154 & answer 3 quick questions."
     end
   end
 
