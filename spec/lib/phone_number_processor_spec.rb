c716b52e4d73e004181ed3ff8353928100944ff1
Dave Guarino <dave@codeforamerica.org>
2014-12-22 16:12:55 -0800

    Implement PhoneNumberProcessor#twilio_number?

--- spec/lib/phone_number_processor_spec.rb
+++ spec/lib/phone_number_processor_spec.rb
@@ -1,13 +1,33 @@
 require File.expand_path('../../phone_number_processor_spec_helper', __FILE__)
 
 describe PhoneNumberProcessor do
-  describe '#language_for' do
-    before(:each) do
-      VCR.use_cassette('phone_number_api_call') do
-        @pnp = PhoneNumberProcessor.new
+  before(:each) do
+    VCR.use_cassette('phone_number_api_call') do
+      @pnp = PhoneNumberProcessor.new
+    end
+  end
+
+  describe '#twilio_number?' do
+    context 'given a phone number that is a known Twilio number' do
+      let(:twilio_phone_number) { '+14151112222' }
+
+      it 'returns true' do
+        result = @pnp.twilio_number?(twilio_phone_number)
+        expect(result).to eq(true)
       end
     end
 
+    context 'given a phone number that is a known Twilio number' do
+      let(:not_a_twilio_phone_number) { '+10009998888' }
+
+      it 'returns true' do
+        result = @pnp.twilio_number?(not_a_twilio_phone_number)
+        expect(result).to eq(false)
+      end
+    end
+  end
+
+  describe '#language_for' do
     context "given a phone number with 'spanish' in its friendly name" do
       let(:twilio_phone_number) { '+14151112222' }
 
