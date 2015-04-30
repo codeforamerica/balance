3cf9e871a5226a20eccd98f6b1175b26d8134828
Dave Guarino <dave@codeforamerica.org>
2015-04-30 15:46:28 -0700

    wip - move fixtures to right place

--- spec/app_spec_helper.rb
+++ spec/app_spec_helper.rb
@@ -11,7 +11,7 @@ class EbtBalanceSmsApp < Sinatra::Base
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
 
--- spec/phone_number_processor_spec_helper.rb
+++ spec/phone_number_processor_spec_helper.rb
@@ -4,6 +4,6 @@ require 'twilio-ruby'
 require 'vcr'
 
 VCR.configure do |c|
-  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
 end
