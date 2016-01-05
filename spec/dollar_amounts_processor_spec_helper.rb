d63910c9fd76a5ba8e69615dc7f33f0f58a44394
Niklas Hofer <niklas+dev@lanpartei.de>
2016-01-05 05:46:11 +0100

    files to hold the processor

---
+++ spec/dollar_amounts_processor_spec_helper.rb
@@ -0,0 +1,3 @@
+require File.expand_path('../spec_helper', __FILE__)
+require File.expand_path('../../lib/dollar_amounts_processor', __FILE__)
+
---
+++ spec/lib/dollar_amounts_processor_spec.rb
@@ -0,0 +1,5 @@
+require File.expand_path('../../dollar_amounts_processor_spec_helper', __FILE__)
+
+describe DollarAmountsProcessor do
+
+end
