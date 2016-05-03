5f2e8d798244d56e2dcdf990eff433eaa9c8d7a7
Yami <yamilethmedina1@gmail.com>
2016-05-02 20:43:03 -0400

    resolving merge conflict - FL is ready to go!

--- lib/state_handler/fl.rb
+++ lib/state_handler/fl.rb
@@ -15,22 +15,6 @@ class StateHandler::FL < StateHandler::Base
     "wwww1wwww#{ebt_number}"
   end
 
-# =begin # Delete this line when ready to transcribe!
-
-# Step 0. Change "::Example" below to a state abbreviation
-# For example, "::PA" for Pennsylvania
-class StateHandler::FL < StateHandler::Base
-
-  # Step 1. EXAMPLE — Edit for your state!
-  PHONE_NUMBER = '+8637346192'
-
-  # Step 2. EXAMPLE — Edit for your state!
-  ALLOWED_NUMBER_OF_EBT_CARD_DIGITS = [16]
-
-  def button_sequence(ebt_number)
-    # Step 3. EXAMPLE — Edit for your state!
-    "ww1ww#{ebt_number}"
-  end
 
   def transcribe_balance_response(transcription_text, language = :english)
     mg = MessageGenerator.new(language)
@@ -42,8 +26,6 @@ class StateHandler::FL < StateHandler::Base
     end
 
     # Deal with an invalid card number
-    ### Step 5. EXAMPLE — Edit for your state! ###
-    phrase_indicating_invalid_card_number = "CHANGE ME"
     ### Step 4. EXAMPLE — Edit for your state! ###
     phrase_indicating_invalid_card_number = "we did not get your card number"
 
@@ -52,10 +34,9 @@ class StateHandler::FL < StateHandler::Base
     end
 
     # Deal with a successful balance transcription
-    ### Step 6. EXAMPLE — Edit for your state! ###
+    ### Step 5. EXAMPLE — Edit for your state! ###
     regex_matches = transcription_text.scan(/(\$\S+)/)
     if regex_matches.count > 0
-    ### Step 5. EXAMPLE — Edit for your state! ###
     regex_matches = transcription_text.scan(/(\$\S+)/)
     if regex_matches.count > 1
       ebt_amount = regex_matches[0][0]
@@ -65,8 +46,6 @@ class StateHandler::FL < StateHandler::Base
     # Deal with any other transcription (catching weird errors)
     # You do not need to change this. :D
     return mg.having_trouble_try_again_message
+    end
   end
-
-# =end # Delete this line when ready to transcribe
-
 end
