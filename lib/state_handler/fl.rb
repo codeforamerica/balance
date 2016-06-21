691946f64f431168bc49ece898b0a0c7ce670628
Yami <yamilethmedina1@gmail.com>
2016-06-20 20:40:35 -0400

    passed tests again after rebase

--- lib/state_handler/fl.rb
+++ lib/state_handler/fl.rb
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 # -*- encoding : utf-8 -*-
-=======
->>>>>>> added FL handler.
-=======
-# -*- encoding : utf-8 -*-
->>>>>>> new Twilio phone number
 # Step 1. Change "::Example" below to a state abbreviation
 # For example, "::PA" for Pennsylvania
 class StateHandler::FL < StateHandler::Base
@@ -21,11 +14,7 @@ class StateHandler::FL < StateHandler::Base
     "wwww1wwww#{ebt_number}"
   end
 
-<<<<<<< HEAD
-=begin # Delete this line when ready to transcribe!
-=======
 
->>>>>>> added FL handler.
   def transcribe_balance_response(transcription_text, language = :english)
     mg = MessageGenerator.new(language)
 
@@ -36,37 +25,16 @@ class StateHandler::FL < StateHandler::Base
     end
 
     # Deal with an invalid card number
-<<<<<<< HEAD
-<<<<<<< HEAD
-    ### Step 5. EXAMPLE — Edit for your state! ###
-    phrase_indicating_invalid_card_number = "CHANGE ME"
-
-=======
-
-    ### Step 5. EXAMPLE — Edit for your state! ###
-    phrase_indicating_invalid_card_number = "CHANGE ME"
-=======
     ### Step 4. EXAMPLE — Edit for your state! ###
     phrase_indicating_invalid_card_number = "we did not get your card number"
->>>>>>> passes all vcr tests!
 
 
->>>>>>> added FL handler.
     if transcription_text.include?(phrase_indicating_invalid_card_number)
       return mg.card_number_not_found_message
     end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    # Deal with a successful balance transcription
-    ### Step 6. EXAMPLE — Edit for your state! ###
-=======
-      ### Step 6. EXAMPLE — Edit for your state! ###
->>>>>>> added FL handler.
-=======
     # Deal with a successful balance transcription
     ### Step 5. EXAMPLE — Edit for your state! ###
->>>>>>> passes all vcr tests!
     regex_matches = transcription_text.scan(/(\$\S+)/)
     if regex_matches.count > 0
     regex_matches = transcription_text.scan(/(\$\S+)/)
@@ -80,8 +48,4 @@ class StateHandler::FL < StateHandler::Base
     return mg.having_trouble_try_again_message
     end
   end
-<<<<<<< HEAD
-=end # Delete this line when ready to transcribe
-=======
->>>>>>> added FL handler.
 end
