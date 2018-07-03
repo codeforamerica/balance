2bbed2cd2e266866a08bfd3130cb67b31540b0c0
Dave Guarino <dave@codeforamerica.org>
2018-07-02 19:43:12 -0700

    elongate waits

--- lib/state_handler/ca.rb
+++ lib/state_handler/ca.rb
@@ -4,7 +4,7 @@ class StateHandler::CA < StateHandler::Base
 
   def button_sequence(ebt_number)
     waiting_ebt_number = ebt_number.split('').join('ww')
-    "wwwwwwww1wwwwwwww#{waiting_ebt_number}ww#ww"
+    "wwwwwwwwwwwwwwww1wwwwwwwwwwwwww#{waiting_ebt_number}ww#wwww"
   end
 
   def transcribe_balance_response(transcription_text, language = :english)
